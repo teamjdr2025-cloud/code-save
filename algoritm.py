5596a52cc5154a9d5c2937bbd5e1cb1e7767f69c
teamjdr2025-cloud <team.jdr.2025@gmail.com>
2025-08-28 00:59:14 +0900

    아리토스테네스의 체 알고리즘

--- algoritm.py
+++ algoritm.py
@@ -1,6 +1,5 @@
 
-nums = []
-
+nums = {}
 def ip():
     n = int(input("소수 판별할 숫자 범위의 최소값을 입력해 주세요: "))
     m = int(input("소수 판별할 숫자 범위의 최대값을 입력해 주세요: "))
@@ -8,15 +7,19 @@ def ip():
         print('이럼 안되여')
         ip()
     else:
-        for i in range(m - n):
-            num = i + n
-            nums.append(num)
-            print(num)
-            if n + i == m - 1 :
-                print(m)
-                nums.append(m)
-                print(nums)
-                
-                break
+        for i in range(2, m + 1):
+            if i < n:
+                    continue
+            for o in range(2, m + 1):
+                if i % o == 0:
+                    if i != o:
+                        nums[i] = '합성수'
+                        break
+                    else:
+                        continue
+                else:
+                    nums[i] = '소수'
+                    continue
+            print(i,'는 ',nums[i],'이다')
 
 ip()
